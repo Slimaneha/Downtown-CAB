2c05d6797e557c16933313e165e92b7851308313
slimane hassani <Slimane>
2018-07-30 14:59:38 +0200

    MAj

--- js/entrainement.js
+++ js/entrainement.js
@@ -5,272 +5,408 @@
 
 var questionUn = {
     question: "Quelle est la limitation de vitesse en villes ?",
-    a :"50KLM",
-    b:"100KLM",
-    c :"90KLM",
-    
+    a: "50KLM",
+    b: "100KLM",
+    c: "90KLM",
+
 
 }
 
 var question2 = {
     question: "Quelle vitesse sur une départementale ?",
-    a :"150KLM",
-    b:"130KLM",
-    c :"100KLM",
-    
+    a: "150KLM",
+    b: "130KLM",
+    c: "100KLM",
+
 
 }
 
 
 var question3 = {
     question: "Que dois je faire quand le feu et rouge ?",
-     a :"Je fonce et on verra ",
-     b:"Je marque un temps d’arrêt et je passe ",
-     c :"J’attends qui passe au vert ",
-    
+    a: "Je fonce et on verra ",
+    b: "Je marque un temps d’arrêt et je passe ",
+    c: "J’attends qui passe au vert ",
+
 
 }
 
 
 
 var question4 = {
-    question: "dddddddddddd",
-    reponcesUn :"dddddddd",
-    reponces2:"dddddddddd",
-    reponces3 :"ddddddddddddd",
-    
+    question: "J'attends des sirènes  ( Police, EMS , FBI ) quelles dois être ma réaction",
+    a: " Je garde mon allure chacun son tour il attendra !!",
+    b: " J'accélère",
+    c: "je laisse passer",
+
 
 }
 
 
 
 var question5 = {
-    question: "5Aarabaji Karim",
-    reponcesUn :"ddddddddddddddd",
-    reponces2:"ddddddddddddddddd",
-    reponces3 :"dddddddddddddddd",
-    
+    question: "Le permis provisoir à une durée de ? ",
+    a: "1 Semaine",
+    b: "1 Mois ",
+    c: "3 Mois ",
+
 
 }
 
 var question = document.getElementById('question')
-var reponces1 = document.getElementById("reponcesUn"); 
-var reponces2 = document.getElementById("reponces2"); 
-var reponces3 = document.getElementById("reponces3"); 
+var reponces1 = document.getElementById("reponcesUn");
+var reponces2 = document.getElementById("reponces2");
+var reponces3 = document.getElementById("reponces3");
 
 
 
+reponcesA = document.getElementById("a")
+reponcesB = document.getElementById("b")
+reponcesC = document.getElementById("c")
+reponcesFinal = ""
 
-function entrainement () {
-    document.getElementById("autoUn").style.display="none"
-    document.getElementById("entrainement").style.display="block"
-    document.getElementById("btnAutre").style.display="none"
-    document.getElementById("btnentrainementUn").style.display="block"
-    question.innerHTML=questionUn["question"]; 
-   reponces1.innerHTML=questionUn["a"]
-   reponces2.innerHTML=questionUn["b"]
-   reponces3.innerHTML=questionUn["c"]
-   document.getElementById("points").style.display="block"
-   document.getElementById("Points").innerHTML = point
+userName = " "
+userName =""
 
 
-}
 
-point = 0 
+point = 0
 erreur = 0
 
-reponcesFinal= ""
+function user() {
 
 
-function autoUn () {
 
-    reponcesFinal= document.getElementById("formAuto").value
-   
-
-  
-    
-  
-    if (reponcesFinal === "C" || reponcesFinal === "c"  ) {
+    document.getElementById("autoUn").style.display = "none"
+    document.getElementById("user").style.display = "block"
+    document.getElementById("btnAutre").style.display = "none"
+    document.getElementById("entrainementUn").style.display="block"
 
 
-        question.innerHTML=question2["question"]
-        reponces1.innerHTML=question2["a"]
-        reponces2.innerHTML=question2["b"]
-        reponces3.innerHTML=question2["c"]
-        point ++
-        document.getElementById("Points").innerHTML = point
-        document.getElementById("btnentrainementUn").style.display="none"
-        document.getElementById("btnentrainement2").style.display="block"
+
+
+}
+
+function entrainement() {
+    document.getElementById("entrainement").style.display = "block"
+    document.getElementById("entrainementUn").style.display="none"
+
+    username = document.getElementById("userUn").value
+    userName = document.getElementById("user2").value
+    document.getElementById("btnentrainementUn").style.display = "block"
+    question.innerHTML = questionUn["question"];
+    reponcesA.innerHTML = questionUn["a"]
+    reponcesB.innerHTML = questionUn["b"]
+    reponcesC.innerHTML = questionUn["c"]
+    document.getElementById("user").style.display = "none"
+
+
+
+}
+
+
+
+
+var selectElmt = document.getElementById("formAuto");
+
+
+
+function autoUn() {
+
+    reponcesFinal = selectElmt.options[selectElmt.selectedIndex].text;
+
+
+
+
+
+
+
+
+    if (reponcesFinal === "90KLM") {
+
+
+        question.innerHTML = question2["question"]
+        reponcesA.innerHTML = question2["a"]
+        reponcesB.innerHTML = question2["b"]
+        reponcesC.innerHTML = question2["c"]
+        point++
+
+        document.getElementById("entrainementUn").style.display = "none"
+        document.getElementById("btnentrainementUn").style.display = "none"
+        document.getElementById("btnentrainement2").style.display = "block"
+        reponcesFinal = ""
 
 
 
     }
     else {
-        question.innerHTML=question2["question"]
-        reponces1.innerHTML=question2["a"]
-        reponces2.innerHTML=question2["b"]
-        reponces3.innerHTML=question2["c"]
-        document.getElementById("Points").innerHTML = point
-        document.getElementById("btnentrainementUn").style.display="none"
-        document.getElementById("btnentrainement2").style.display="block"
-        erreur ++
+        question.innerHTML = question2["question"]
+        reponcesA.innerHTML = question2["a"]
+        reponcesB.innerHTML = question2["b"]
+        reponcesC.innerHTML = question2["c"]
+
+        document.getElementById("btnentrainementUn").style.display = "none"
+        document.getElementById("btnentrainement2").style.display = "block"
+        document.getElementById("btnentrainementUn").style.display = "none"
+
+        erreur++
     }
 
 }
 
 
-function auto2 () {
+function auto2() {
 
-    reponcesFinal= document.getElementById("formAuto").value
-   
-
-  
-    
-  
-    if (reponcesFinal === "B" || reponcesFinal === "b"  ) {
+    reponcesFinal = selectElmt.options[selectElmt.selectedIndex].text;
 
 
-        question.innerHTML=question3["question"]
-        reponces1.innerHTML=question3["a"]
-        reponces2.innerHTML=question3["b"]
-        reponces3.innerHTML=question3["c"]
-        point ++
-        document.getElementById("Points").innerHTML = point
-        document.getElementById("btnentrainement2").style.display="none"
-        document.getElementById("btnentrainement3").style.display="block"
+
+
+
+    if (reponcesFinal === "130KLM") {
+
+
+        question.innerHTML = question3["question"]
+        reponcesA.innerHTML = question3["a"]
+        reponcesB.innerHTML = question3["b"]
+        reponcesC.innerHTML = question3["c"]
+        point++
+
+        document.getElementById("btnentrainement2").style.display = "none"
+        document.getElementById("btnentrainement3").style.display = "block"
 
 
 
     }
     else {
-        question.innerHTML=question3["question"]
-        reponces1.innerHTML=question3["a"]
-        reponces2.innerHTML=question3["b"]
-        reponces3.innerHTML=question3["c"]
+        question.innerHTML = question3["question"]
+        reponcesA.innerHTML = question3["a"]
+        reponcesB.innerHTML = question3["b"]
+        reponcesC.innerHTML = question3["c"]
+
+
+        document.getElementById("btnentrainement2").style.display = "none"
+        document.getElementById("btnentrainement3").style.display = "block"
+        erreur++
+    }
+
+}
+
+
+function auto3() {
+
+
+
+
+
+    reponcesFinal = selectElmt.options[selectElmt.selectedIndex].text;
+
+    if (reponcesFinal === "Je marque un temps d’arrêt et je passe") {
+
+
+        question.innerHTML = question4["question"]
+        reponcesA.innerHTML = question4["a"]
+        reponcesB.innerHTML = question4["b"]
+        reponcesC.innerHTML = question4["c"]
+        point++
+
+        document.getElementById("btnentrainement3").style.display = "none"
+        document.getElementById("btnentrainement4").style.display = "block"
+
+
+
+    }
+    else {
+
+        question.innerHTML = question4["question"]
+        reponcesA.innerHTML = question4["a"]
+        reponcesB.innerHTML = question4["b"]
+        reponcesC.innerHTML = question4["c"]
+        document.getElementById("btnentrainement3").style.display = "none"
+        document.getElementById("btnentrainement4").style.display = "block"
+        erreur++
+    }
+
+}
+
+
+function auto4() {
+
+
+    reponcesFinal = selectElmt.options[selectElmt.selectedIndex].text;
+
+
+
+    if (reponcesFinal === "je laisse passer") {
+
+
+        question.innerHTML = question5["question"]
+        reponcesA.innerHTML = question5["a"]
+        reponcesB.innerHTML = question5["b"]
+        reponcesC.innerHTML = question5["c"]
+        point++
+
+        document.getElementById("btnentrainement4").style.display = "none"
+        document.getElementById("btnentrainement5").style.display = "block"
+
+
+
+    }
+
+    else {
+        question.innerHTML = question5["question"]
+        reponcesA.innerHTML = question5["a"]
+        reponcesB.innerHTML = question5["b"]
+        reponcesC.innerHTML = question5["c"]
+        erreur++
+
+        document.getElementById("btnentrainement4").style.display = "none"
+        document.getElementById("btnentrainement5").style.display = "block"
+    }
+
+}
+
+
+
+
+boot = ""
+
+//var dateD = date ()
+
+
+var codeBare = document.getElementById("code")
+var d = new Date();
+
+
+function auto5() {
+    reponcesFinal = selectElmt.options[selectElmt.selectedIndex].text;
+    document.getElementById("name").innerHTML= username;
+    document.getElementById("nameDeux").innerHTML = userName
+   
+    boot = ""
+    if (point >= 4 ) {
+        var boot = Math.random(); //valeur aleatoire entre 0 et 1//
+    //On traduit ce nombre aleatoire en un choix aleatoire entre pierre, papier, ciseaux//
+    if (boot < 0.34) {
+        boot = "SL" + d.getTime() ;
+        codeBare.innerHTML = boot
+
         
-        document.getElementById("Points").innerHTML = point
-        document.getElementById("btnentrainement2").style.display="none"
-        document.getElementById("btnentrainement3").style.display="block"
-        erreur ++
-    }
-
-}
-
-
-function auto3 () {
-
-    reponcesFinal= document.getElementById("formAuto").value
-   
-
-  
-    
-  
-    if (reponcesFinal === "B" || reponcesFinal === "b"  ) {
-
-
-        question.innerHTML=question4["question"]
-        reponces1.innerHTML=question4["a"]
-        reponces2.innerHTML=question4["b"]
-        reponces3.innerHTML=question4["c"]
-        point ++
-        document.getElementById("Points").innerHTML = point
-        document.getElementById("btnentrainement3").style.display="none"
-        document.getElementById("btnentrainement4").style.display="block"
-
-
-
-    }
-    else {
         
-        question.innerHTML=question4["question"]
-        reponces1.innerHTML=question4["a"]
-        reponces2.innerHTML=question4["b"]
-        reponces3.innerHTML=question4["c"]
-        document.getElementById("Points").innerHTML = point
-        document.getElementById("btnentrainement3").style.display="none"
-        document.getElementById("btnentrainement4").style.display="block"
-        erreur ++
+    }
+    else if (boot <= 0.67) {
+        boot = "SH" + d.getTime() ;
+        codeBare.innerHTML = boot
+        
+
+    }
+    else {
+        boot = "AB" + d.getTime();
+        codeBare.innerHTML = boot
+
+    }
     }
 
-}
-
-
-function auto4 () {
-
-    reponcesFinal= document.getElementById("formAuto").value
-   
-
-  
     
-  
-    if (reponcesFinal === "B" || reponcesFinal === "b"  ) {
 
 
-        question.innerHTML=question5["question"]
-        reponces1.innerHTML=question5["a"]
-        reponces2.innerHTML=question5["b"]
-        reponces3.innerHTML=question5["c"]
-        point ++
-        document.getElementById("Points").innerHTML = point
-        document.getElementById("btnentrainement4").style.display="none"
-        document.getElementById("btnentrainement5").style.display="block"
+    else if (point < 4 ) {
 
-
-
+        var boot = Math.random(); //valeur aleatoire entre 0 et 1//
+        //On traduit ce nombre aleatoire en un choix aleatoire entre pierre, papier, ciseaux//
+        if (boot < 0.34) {
+            boot = "BA";
+            codeBare.innerHTML = boot + d.getTime()
+            
+            
+        }
+        else if (boot <= 0.67) {
+            boot = "FR";
+            codeBare.innerHTML = boot + d.getTime()
+            
+    
+        }
+        else {
+            boot = "TG";
+            codeBare.innerHTML = boot + d.getTime()
+            
+    
+        }
     }
+
+    if (reponcesFinal === "1 Semaine") {
+
+        question.style.display = "none"
+
+
+        document.getElementById("entrainement").style.display = "none"
+        document.getElementById("result").style.display = "block"
+        
+
+        document.getElementById("btnentrainement5").style.display = "none"
+        document.getElementById("btnentrainement6").style.display = "block"
+        point++
+
+
+      
+        }
+    
+
+
 
     else {
-        question.innerHTML=question5["question"]
-        reponces1.innerHTML=question5["a"]
-        reponces2.innerHTML=question5["b"]
-        reponces3.innerHTML=question5["c"]
-        erreur ++
-        document.getElementById("Points").innerHTML = point
-        document.getElementById("btnentrainement4").style.display="none"
-        document.getElementById("btnentrainement5").style.display="block"
+        question.style.display = "none"
+
+        document.getElementById("entrainement").style.display = "none"
+        document.getElementById("result").style.display = "block"
+
+        document.getElementById("btnentrainement5").style.display = "none"
+        document.getElementById("btnentrainement6").style.display = "block"
+        erreur++
+      
+        
+        
     }
 
-}
 
 
 
- function auto5 () {
-      question.style.display="none"
-        reponces1.style.display="none"
-        reponces2.style.display="none"
-        reponces3.style.display="none"
-        document.getElementById("erreur").innerHTML=erreur; 
-        document.getElementById("pointsUn").innerHTML=point;
-        document.getElementById("entrainement").style.display="none"
-        document.getElementById("result").style.display="block"
-        document.getElementById("Points").innerHTML = point
-        document.getElementById("btnentrainement5").style.display="none"
-        document.getElementById("btnentrainement6").style.display="block"
 
 
 
-   
-
-   
 }
 
 
 
 
-function auto6 () {
-
-
-    document.getElementById("result").style.display="none"; 
-    document.getElementById("autoUn").style.display="block"
-    document.getElementById("entrainement").style.display="none"
-    document.getElementById("btnentrainement6").style.display="none"
-    document.getElementById("btnAutre").style.display="block"
-    document.getElementById("Points").style.display="none"
-    document.getElementById("points").style.display="none"
 
 
 
-    point = 0 
+
+
+
+
+function auto6() {
+
+
+    document.getElementById("result").style.display = "none";
+    document.getElementById("autoUn").style.display = "block"
+    document.getElementById("entrainement").style.display = "none"
+    document.getElementById("btnentrainement6").style.display = "none"
+    document.getElementById("btnAutre").style.display = "block"
+
+
+
+
+
+
+
+
+
+
+
+
+    point = 0
     erreur = 0
 }
 
@@ -281,40 +417,43 @@ function auto6 () {
 
 var forfaitAuto = {
 
-        voiture :"500 $", 
-        Moto : "500 $",
-        Camion: "300$", 
-        Bateau : "700$", 
-        promoEnCours : "20% Pour tout Nouveaux arrivant"
-        
-
-
-}
-
-function PrixAuto () {
-
-    document.getElementById("prixAuto").style.display="block"
-    document.getElementById("voiture").innerHTML=forfaitAuto["voiture"]
-    document.getElementById("moto").innerHTML=forfaitAuto["Moto"]
-    document.getElementById("camion").innerHTML=forfaitAuto["Camion"]
-    document.getElementById("bateau").innerHTML=forfaitAuto["Bateau"]
-    document.getElementById("autoUn").style.display="none"
-    document.getElementById("btnentrainement7").style.display="block"
-    document.getElementById("btnAutre").style.display="none"
-
+    voiture: "500 $",
+    Moto: "500 $",
+    Camion: "300$",
+    Bateau: "700$",
+    promoEnCours: "20% Pour tout Nouveaux arrivant"
 
 
 
 }
 
+function PrixAuto() {
 
-function auto7 () {
-    document.getElementById("prixAuto").style.display="none"
-    document.getElementById("btnAutre").style.display="block"
-    document.getElementById("autoUn").style.display="block"
-    document.getElementById("btnentrainement7").style.display="none"
+    document.getElementById("prixAuto").style.display = "block"
+    document.getElementById("voiture").innerHTML = forfaitAuto["voiture"]
+    document.getElementById("moto").innerHTML = forfaitAuto["Moto"]
+    document.getElementById("camion").innerHTML = forfaitAuto["Camion"]
+    document.getElementById("bateau").innerHTML = forfaitAuto["Bateau"]
+    document.getElementById("autoUn").style.display = "none"
+    document.getElementById("btnentrainement7").style.display = "block"
+    document.getElementById("btnAutre").style.display = "none"
 
 
 
 
 }
+
+
+
+
+function auto7() {
+    document.getElementById("prixAuto").style.display = "none"
+    document.getElementById("btnAutre").style.display = "block"
+    document.getElementById("autoUn").style.display = "block"
+    document.getElementById("btnentrainement7").style.display = "none"
+
+}
+
+// Vailidé 
+
+
